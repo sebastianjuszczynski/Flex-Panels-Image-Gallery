704f9fdd32465f4d81cde24b14aa4ee7d796f47f
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-25 20:27:36 +0100

    added function toggleOpen

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+    this.classList.toggle("open");
+}
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
